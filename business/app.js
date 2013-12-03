d7655acabceb68449753f1aba36bae8dea811be7
Willy Aguirre <marti1125@gmail.com>
2013-12-02 22:02:13 -0500

    update proyect

--- business/app.js
+++ business/app.js
@@ -3,64 +3,45 @@
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes')
-  , company = require('./routes/company')
-  , solutions = require('./routes/solutions')
-  , services = require('./routes/services')
-  , contacts = require('./routes/contacts')
-  , http = require('http')
-  , path = require('path');
-
+var express = require('express');
+var routes = require('./routes');
+var http = require('http');
+var path = require('path');
+var company = require('./routes/company');
+var solutions = require('./routes/solutions');
+var services = require('./routes/services');
+var contacts = require('./routes/contacts');
 var app = express();
 
-app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.cookieParser('your secret here'));
-  app.use(express.session());
-  app.use(app.router);
-  app.use(require('stylus').middleware(__dirname + '/public'));
-  app.use(express.static(path.join(__dirname, 'public')));
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(express.cookieParser('your secret here'));
+app.use(express.session());
+app.use(app.router);
+app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-  app.use(function(req, res, next){
-    res.status(404);
-
-    // respond with html page
-    if (req.accepts('html')) {
-      res.render('404', { url: req.url });
-      return;
-    }
-
-    // default to plain-text. send()
-    res.type('txt').send('Not found');
-
-  });
-
-});
-
-app.configure('development', function(){
+// development only
+if ('development' == app.get('env')) {
   app.use(express.errorHandler());
-});
+}
 
 app.get('/', routes.index);
 app.get('/company', company.company);
 app.get('/solutions', solutions.solutions);
 app.get('/services', services.services);
 app.get('/contacts', contacts.contacts);
-app.get('/404', function(req, res, next){
-  next();
-});
-
-app.post('/send', function(req, res){
-	
+app.use(function(req,res){
+    res.render('404');
 });
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+  console.log('Express server listening on port ' + app.get('port'));
 });
